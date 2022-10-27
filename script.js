47d7e4f92369ba545ad385918352e6e5e9830323
James Crutchley <jamescrutchley98@gmail.com>
2022-10-27 20:20:42 +1100

    Add navigation toggle to header

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const CloseCta = () => {
     popup.style.display = 'none';
 };
 
+
+const ToggleNav = () => {
+    const nav = document.getElementById('nav-main');
+    if (nav.style.display === 'none') {
+        nav.style.display = 'block';
+    } else {
+        nav.style.display = 'none';
+    }
+};
+
+
