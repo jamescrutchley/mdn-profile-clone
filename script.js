a5e529bb59a5ea06d26da1a68f6d63d36af945dc
James Crutchley <jamescrutchley98@gmail.com>
2022-10-28 17:19:15 +1100

    Create main navigation sections

--- script.js
+++ script.js
@@ -14,4 +14,53 @@ const ToggleNav = () => {
     }
 };
 
+// const ToggleSubNav = (id) => {
+//     const subNav = document.getElementById(id);
+//     if (subNav.style.display === 'none') {
+//         subNav.style.display = 'block';
+//     } else {
+//         subNav.style.display = 'none';
+//     }
+// };
 
+const ToggleCategories = () => {
+    const nav = document.getElementById('nav-main');
+    if (nav.style.display === 'none') {
+        nav.style.display = 'block';
+    } else {
+        nav.style.display = 'none';
+    }
+};
+
+
+//  make generic and onload.
+
+document.getElementById('nav-references-button').addEventListener("click", function() {
+    const subcategory = document.getElementById('nav-references');
+    const icon = document.getElementById(`nav-references-icon`);
+    if (subcategory.style.display === 'none') {
+        subcategory.style.display = 'block';
+        icon.style.transform = 'rotate(90deg)';
+      } else {
+        subcategory.style.display = 'none';
+        icon.style.transform = 'rotate(270deg)';
+    }
+})
+
+document.getElementById('nav-guides-button').addEventListener("click", function() {
+    const subcategory = document.getElementById('nav-guides');
+    if (subcategory.style.display === 'none') {
+        subcategory.style.display = 'block';
+      } else {
+        subcategory.style.display = 'none';
+    }
+})
+
+document.getElementById('nav-premium-button').addEventListener("click", function() {
+    const subcategory = document.getElementById('nav-premium');
+    if (subcategory.style.display === 'none') {
+        subcategory.style.display = 'block';
+      } else {
+        subcategory.style.display = 'none';
+    }
+})
